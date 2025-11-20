1469eb71e580fffece7d22cedfcd623063964b77
Wu Chun Ming <ming374@1utar.my>
2025-11-20 21:03:33 +0800

    Initial commit

---
+++ src/test/java/io/github/wcm/schedulemanager/SchedulemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wcm.schedulemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
