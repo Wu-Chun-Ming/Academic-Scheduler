c842a3d4f53d2568b1d989fd19ff9155ab78a027
Wu Chun Ming <ming374@1utar.my>
2025-11-27 21:56:27 +0800

    Refactor ScheduleRequestDto to use LocalDateTime for date fields

--- src/main/java/io/github/wcm/schedulemanager/dto/ScheduleRequestDto.java
+++ src/main/java/io/github/wcm/schedulemanager/dto/ScheduleRequestDto.java
@@ -1,7 +1,10 @@
 package io.github.wcm.schedulemanager.dto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,12 +13,13 @@ import lombok.Getter;
 
 @Getter
 public class ScheduleRequestDto {
-	@NotNull(message = "Start date is required")
-	private LocalDate startDate;
-	private LocalDate endDate;
+	@NotNull(message = "Start time is required")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	private LocalDateTime startDateTime;
 
-	private LocalTime startTime;
-	private LocalTime endTime;
+	@NotNull(message = "End time is required")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	private LocalDateTime endDateTime;
 
 	@NotBlank(message = "Course code is required")
 	@Size(min = 7, max = 8, message = "Course code must be 7-8 characters")
@@ -34,4 +38,20 @@ public class ScheduleRequestDto {
 
 	@NotBlank(message = "Scope is required")
 	private String scope;
+
+	public LocalDate getStartDate() {
+		return startDateTime.toLocalDate();
+	}
+	
+	public LocalTime getStartTime() {
+		return startDateTime.toLocalTime();
+	}
+
+	public LocalDate getEndDate() {
+		return endDateTime.toLocalDate();
+	}
+	
+	public LocalTime getEndTime() {
+		return endDateTime.toLocalTime();
+	}
 }
